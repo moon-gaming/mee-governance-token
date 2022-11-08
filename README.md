d93f710e867d1f230f10bd5a20b5f50cf941e431
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2022-11-09 06:25:45 +0900

    Removing obsolete code

--- README.md
+++ README.md
@@ -2,34 +2,13 @@ Governance Token(MEE) to trade in Medieval Empires
 
 COMPILE
 
-> npx hardhat compile
+> yarn hardhat compile
 
 RUN TESTS
 
-> npx hardhat test  
-> npm run coverage:local `with coverage`
+> yarn test
+> yarn coverage:local
 
 DEPLOY CONTRACTS TO SPECIFIED NETWORK
 
 > npx hardhat run scripts/deploy.js --network #network-name  `sample: ropsten, matic, look at networks in hardhat.config.js`
-
-SAMPLE COMMANDS
-
-> npx hardhat accounts  
-> npx hardhat setGameOwner --address 0x41F30b058ec6aff3d858eE16FC16B9275affdb81 `optional command`  
-> npx hardhat getGameOwner `optional command`
-> npx hardhat getsignatory
-> npx hardhat balance  
-> npx hardhat setTokenPrice --round seed --price 150
-> npx hardhat setActiveRound --round exhange  
-> npx hardhat addAddress --round seed --address 0x00..
-> npx hardhat deleteAddress --round seed --address 0x00.. --index 0
-> npx hardhat setActiveRound --round private  
-> npx hardhat addAddress --round private --address '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199,0xdD2FD4581271e230360230F9337D5c0430Bf44C0'  
-> npx hardhat getAddressList --round private  
-> npx hardhat deleteAddress --round private --address 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199 --index 0  
-> npx hardhat reserveTokens --round private --to 0xBcd4042DE499D14e55001CcbB24a551F3b954096 --amount 10  
-> npx hardhat totalPending  --round private --to 0xBcd4042DE499D14e55001CcbB24a551F3b954096  
-> npx hardhat totalRemainingForSpecificRound --round private  
-> npx hardhat totalRemainingForAllRounds  
-> npx hardhat totalClaimedForAllRounds  
--- test/token/AuxiliaryTests.ts
+++ test/token/AuxiliaryTests.ts
@@ -9,6 +9,7 @@ const pow18 = BigNumber.from("10").pow(18);
 enum RoundType {
     SEED, PRIVATE, PUBLIC, PLAYANDEARN, EXCHANGES, TREASURY, ADVISOR, TEAM, SOCIAL
 }
+
 const rounds = [RoundType.SEED, RoundType.PRIVATE, RoundType.PUBLIC, RoundType.PLAYANDEARN, RoundType.EXCHANGES, RoundType.TREASURY, RoundType.ADVISOR, RoundType.TEAM, RoundType.SOCIAL];
 
 describe("Auxiliary Tests", function () {
@@ -16,7 +17,6 @@ describe("Auxiliary Tests", function () {
     let governanceToken: Contract;
     let owner: SignerWithAddress;
     let gameOwner: SignerWithAddress;
-    let buyer: SignerWithAddress;
     let addrs: SignerWithAddress[];
 
     let publicWallet: SignerWithAddress;
@@ -26,11 +26,11 @@ describe("Auxiliary Tests", function () {
     async function deployToken(purpose: string)
     {
         // Get the ContractFactory and Signers here.
-        var factory = await ethers.getContractFactory("GovernanceToken");
+        let factory = await ethers.getContractFactory("GovernanceToken");
 
         const addressList = addrs.filter((_, index) => index < 7).map((addr) => addr.address);
 
-        var governanceToken = await factory.deploy(
+        let governanceToken = await factory.deploy(
             purpose, 18, "MEE", gameOwner.address, addressList);
 
         return governanceToken;
@@ -39,7 +39,7 @@ describe("Auxiliary Tests", function () {
     // Deploy contract once, and make some quick assumptions.
     before(async () => {
         const accounts = await ethers.getSigners();
-        [owner, gameOwner, buyer,/* signer,*/ ...addrs] = accounts;
+        [owner, gameOwner, ...addrs] = accounts;
 
         publicWallet = addrs[0];
         exchangesWallet = addrs[1];
@@ -50,63 +50,63 @@ describe("Auxiliary Tests", function () {
 
     describe("Pre-Minting", () => {
         it("Public Wallet holds 120M tokens after Deploy", async () => {
-            var balance = await governanceToken.connect(publicWallet).balanceOf(publicWallet.address);
+            let balance = await governanceToken.connect(publicWallet).balanceOf(publicWallet.address);
             expect(balance).to.equal(pow18.mul(120_000_000));
         });
 
         it("Public Wallet has no reserve after Deploy", async () => {
             for (const round of rounds) {
-                var reserved = await governanceToken.connect(publicWallet).getTotalPending(RoundType[round], publicWallet.address);
+                let reserved = await governanceToken.connect(publicWallet).getTotalPending(RoundType[round], publicWallet.address);
                 expect(reserved).to.equal(0, RoundType[round]);
             }
         });
 
         it("Exchanges Wallet holds 60M tokens after Deploy", async () => {
-            var balance = await governanceToken.connect(exchangesWallet).balanceOf(exchangesWallet.address);
+            let balance = await governanceToken.connect(exchangesWallet).balanceOf(exchangesWallet.address);
             expect(balance).to.equal(pow18.mul(60_000_000));
         });
 
         it("Exchanges Wallet has 90M reserved after Deploy", async () => {
-            var reserved = await governanceToken.connect(exchangesWallet).getTotalPending(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
+            let reserved = await governanceToken.connect(exchangesWallet).getTotalPending(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
             expect(reserved).to.equal(pow18.mul(90_000_000));
         });
 
 
         it("Exchanges Wallet may claim nothing after only a half month", async () => {
-            var contract = await deployToken("Exchanges half-monthly Vesting Test");
+            let contract = await deployToken("Exchanges half-monthly Vesting Test");
 
             await contract.connect(gameOwner).beginVesting();
 
             time.increase(time.duration.days(15));
-            var reserved = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
+            let reserved = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
             expect(reserved).to.equal(0);
         });
 
         it("Exchanges Wallet may claim 30M each 30 days", async () => {
-            var contract = await deployToken("Exchanges Monthly Vesting Test");
+            let contract = await deployToken("Exchanges Monthly Vesting Test");
 
             await contract.connect(gameOwner).beginVesting();
 
             for (let i = 1; i <= 3; i++)
             {
                 time.increase(time.duration.days(30));
-                var reserved = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
+                let reserved = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
                 expect(reserved).to.equal(pow18.mul(30_000_000).mul(i), "Month" + i);
             }
         });
 
         it("Exchanges Wallet may claim 90M after 3 months or more", async () => {
-            var contract = await deployToken("Exchanges 90 Day Test");
+            let contract = await deployToken("Exchanges 90 Day Test");
             await contract.connect(gameOwner).beginVesting();
 
             time.increase(time.duration.days(90));
 
-            var reserved = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
-            expect(reserved).to.equal(pow18.mul(90_000_000), "After exactly 90 days.");
+            let reservedExactly = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
+            expect(reservedExactly).to.equal(pow18.mul(90_000_000), "After exactly 90 days.");
 
             time.increase(time.duration.days(1234));
 
-            var reserved = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
+            let reserved = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
             expect(reserved).to.equal(pow18.mul(90_000_000), "After longer time.");
         });
 
