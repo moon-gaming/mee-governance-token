1568a99aeb5be9a5a5ff1f6748da5fbc245cdf5e
Moritz Voss <moritz.voss@moongaming.com>
2022-12-05 12:12:43 +0100

    Claiming tests and Auxiliary tests fixed for new tokenomics - CAVEAT: Bug found for Advisors wallet.

--- test/token/AuxiliaryTests.ts
+++ test/token/AuxiliaryTests.ts
@@ -3,6 +3,7 @@ import {ethers} from "hardhat";
 import {BigNumber, Contract} from "ethers";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {time} from "@nomicfoundation/hardhat-network-helpers";
+import { Console } from "console";
 
 const pow18 = BigNumber.from("10").pow(18);
 
@@ -64,27 +65,27 @@ describe("Auxiliary Tests", function () {
             }
         });
 
-        it("Exchanges Wallet holds 60M tokens after Deploy", async () => {
+        it("Exchanges Wallet holds 34.5M tokens after Deploy", async () => {
             let balance = await governanceToken.connect(exchangesWallet).balanceOf(exchangesWallet.address);
-            expect(balance).to.equal(pow18.mul(60_000_000));
+            expect(balance).to.equal(pow18.mul(34_500_000));
         });
 
-        it("Exchanges Wallet has 90M reserved after Deploy", async () => {
+        it("Exchanges Wallet has 115.5M reserved after Deploy", async () => {
             let reserved = await governanceToken.connect(exchangesWallet).getTotalPending(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
-            expect(reserved).to.equal(pow18.mul(90_000_000));
+            expect(reserved).to.equal(pow18.mul(115_500_000));
         });
 
-        it("Exchanges Wallet may claim 15M after only a half month", async () => {
+        it("Exchanges Wallet may claim 19.25M after only a half month", async () => {
             let contract = await deployToken("Exchanges half-monthly Vesting Test");
 
             await contract.connect(gameOwner).beginVesting();
 
             time.increase(time.duration.days(15));
             let claimable = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
-            expect(claimable).to.equal(pow18.mul(15_000_000));
+            expect(claimable).to.approximately(pow18.mul(19_250_000), 100);
         });
 
-        it("Exchanges Wallet may claim 30M each 30 days", async () => {
+        it("Exchanges Wallet may claim 38.5M each 30 days", async () => {
             let contract = await deployToken("Exchanges Monthly Vesting Test");
 
             await contract.connect(gameOwner).beginVesting();
@@ -93,11 +94,11 @@ describe("Auxiliary Tests", function () {
             {
                 time.increase(time.duration.days(30));
                 let claimable = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
-                expect(claimable).to.equal(pow18.mul(30_000_000).mul(i), "Month" + i);
+                expect(claimable).to.approximately(pow18.mul(38_500_000).mul(i), 100, "Month" + i);
             }
         });
 
-        it("Exchanges Wallet can claim 1M each day for 30 days straight", async () => {
+        it("Exchanges Wallet can claim 1.283M each day for 30 days straight", async () => {
             let contract = await deployToken("Exchanges Daily Vesting Test");
 
             await contract.connect(gameOwner).beginVesting();
@@ -112,7 +113,7 @@ describe("Auxiliary Tests", function () {
                 var increment = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
 
                 //We can claim 1M each day except on day 0
-                expect(increment).to.equal(pow18.mul(1_000_000));
+                expect(increment).to.approximately(pow18.mul(38_500_000).div(30), 100, "Daily claimed.");
                 cumulative = cumulative.add(increment);
 
                 //Actually try to claim.
@@ -120,22 +121,31 @@ describe("Auxiliary Tests", function () {
                 var balance = await contract.connect(exchangesWallet).balanceOf(exchangesWallet.address);
                 expect(balance.sub(oldbalance)).to.equal(cumulative, "Claimed total equals accrued total.");
             }
-            expect(cumulative).to.equal(pow18.mul(30_000_000), "Total claimed daily");
+            expect(cumulative).to.approximately(pow18.mul(38_500_000), 100, "Total claimed daily");
         });
 
-        it("Exchanges Wallet may claim 90M after 3 months or more", async () => {
+        it("Exchanges Wallet may claim 115.5M after 3 months or more", async () => {
             let contract = await deployToken("Exchanges 90 Day Test");
             await contract.connect(gameOwner).beginVesting();
 
             time.increase(time.duration.days(90));
 
             let reservedExactly = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
-            expect(reservedExactly).to.equal(pow18.mul(90_000_000), "After exactly 90 days.");
+            expect(reservedExactly).to.approximately(pow18.mul(115_500_000), 100, "After exactly 90 days.");
 
             time.increase(time.duration.days(1234));
 
             let claimable = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
-            expect(claimable).to.equal(pow18.mul(90_000_000), "After longer time.");
+            expect(claimable).to.equal(pow18.mul(115_500_000), "After longer time.");
+        });
+
+        it("Advisors Wallet has 150M pending", async () => {
+            let contract = await deployToken("Advisors pending test");
+
+            await contract.connect(gameOwner).beginVesting();
+            
+            let claimable = await contract.connect(advisorsWallet).getTotalPending(RoundType[RoundType.ADVISOR], advisorsWallet.address);
+            expect(claimable).to.equal(pow18.mul(150_000_000));
         });
 
         it("Advisors Wallet may claim nothing after half a month", async () => {
@@ -148,16 +158,24 @@ describe("Auxiliary Tests", function () {
             expect(claimable).to.equal(0);
         });
 
-        it("Advisors Wallet may claim 30M each 30 days", async () => {
+        it("Advisors Wallet may claim 7.5M each 30 days after 120 day cliff", async () => {
             let contract = await deployToken("Advisors Monthly Vesting Test");
 
             await contract.connect(gameOwner).beginVesting();
+            let claimable = await contract.connect(advisorsWallet).getClaimableBalance(RoundType[RoundType.ADVISOR], advisorsWallet.address);
+            expect(claimable).to.equal(0, "Nothing should be claimable before cliff has passed.");
+
+            time.increase(time.duration.days(121));
+
+            claimable = await contract.connect(advisorsWallet).getClaimableBalance(RoundType[RoundType.ADVISOR], advisorsWallet.address);
+            expect(claimable).to.equal(0, "Nothing should be claimable before first month after of cliff passed.");
 
             for (let i = 1; i <= 3; i++)
             {
-                time.increase(time.duration.days(30));
-                let claimable = await contract.connect(exchangesWallet).getClaimableBalance(RoundType[RoundType.EXCHANGES], exchangesWallet.address);
-                expect(claimable).to.equal(pow18.mul(30_000_000).mul(i), "Month" + i);
+                console.log("Month " + i);
+                time.increase(time.duration.days(35));
+                claimable = await contract.connect(advisorsWallet).getClaimableBalance(RoundType[RoundType.ADVISOR], advisorsWallet.address);
+                expect(claimable).to.equal(pow18.mul(7_500_000).mul(i), "Month" + i);
             }
         });
     });
