06cd77e0aaac24a032d6dc6c5a178423d97ec04c
muranox <naokimurano@outlook.com>
2023-07-26 17:59:08 +0900

    fix: update silver purchase into sku purchase

--- contracts/game/SkuPurchaseERC20.sol
+++ contracts/game/SkuPurchaseERC20.sol
@@ -5,11 +5,11 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
-contract SilverPurchaseERC20 is AccessControlUpgradeable, PausableUpgradeable {
+contract SkuPurchaseERC20 is AccessControlUpgradeable, PausableUpgradeable {
 
     struct PackageInfo {
         uint256 price;
-        uint256 silver;
+        uint256 amount;
     }
 
     bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
@@ -22,10 +22,10 @@ contract SilverPurchaseERC20 is AccessControlUpgradeable, PausableUpgradeable {
     function initialize(address _usdcTokenAddress) public initializer {
         __Pausable_init_unchained();
         __AccessControl_init_unchained();
-        __SilverPurchaseERC20_init_unchained(_usdcTokenAddress);
+        __SkuPurchaseERC20_init_unchained(_usdcTokenAddress);
     }
 
-    function __SilverPurchaseERC20_init_unchained(address _usdcTokenAddress)
+    function __SkuPurchaseERC20_init_unchained(address _usdcTokenAddress)
         internal
         onlyInitializing
     {
@@ -34,24 +34,27 @@ contract SilverPurchaseERC20 is AccessControlUpgradeable, PausableUpgradeable {
         usdcTokenAddress = _usdcTokenAddress;
 
         // USDC decimals : 6 
-        // 10 USDC for 10,000 Silvers
+        // 10 USDC for 10,000 Silver
         packageInfo["silver-10k"] = PackageInfo(10e6, 10000);
-        // 50 USDC for 50,000 Silvers
+        // 50 USDC for 50,000 Silver
         packageInfo["silver-50k"] = PackageInfo(50e6, 50000);
-        // 100 USDC for 100,000 Silvers
+        // 100 USDC for 100,000 Silver
         packageInfo["silver-100k"] = PackageInfo(100e6, 100000);
-        // 200 USDC for 200,000 Silvers
+        // 200 USDC for 200,000 Silver
         packageInfo["silver-200k"] = PackageInfo(200e6, 200000);
-        // 450 USDC for 500,000 Silvers
+        // 450 USDC for 500,000 Silver
         packageInfo["silver-500k"] = PackageInfo(450e6, 500000);
     }
 
-    function purchase(string memory index, uint256 amount) external whenNotPaused {
-        PackageInfo memory package = packageInfo[index];
-        uint256 usdAmount = package.price * amount;
-        require(package.price > 0 && package.silver > 0, "Package is not purchasable");
+    function purchase(string memory sku, uint256 skuAmount) external whenNotPaused {
+        PackageInfo memory package = packageInfo[sku];
+        require(package.price > 0 && package.amount > 0, "Package is not purchasable");
+
+        uint256 usdAmount = package.price * skuAmount;
+        uint256 amount = package.amount * skuAmount;
+        
         IERC20Upgradeable(usdcTokenAddress).transferFrom(msg.sender, address(this), usdAmount);
-        emit Purchased(msg.sender, index, amount, usdAmount, package.silver);
+        emit Purchased(msg.sender, sku, skuAmount, usdAmount, amount);
     }
 
     function withdraw(address tokenAddress) external onlyRole(WITHDRAW_ROLE) {
@@ -60,9 +63,9 @@ contract SilverPurchaseERC20 is AccessControlUpgradeable, PausableUpgradeable {
         emit Withdrawn(msg.sender, tokenAddress, balance);
     }
 
-    function updatePackage(string memory index, uint256 amount, uint256 silver) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        packageInfo[index].price = amount;
-        packageInfo[index].silver = silver;
+    function updatePackage(string memory sku, uint256 price, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        packageInfo[sku].price = price;
+        packageInfo[sku].amount = amount;
     }
 
     function pause() external onlyRole(DEFAULT_ADMIN_ROLE) {
