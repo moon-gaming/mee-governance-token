f957743732b792dd8a48dbcbff048487c5f02796
supermutecx <naokimurano@outlook.com>
2024-05-09 12:03:01 +0900

    fix: token uri for nft & name, symbol update

--- contracts/game/GenesisHero.sol
+++ contracts/game/GenesisHero.sol
@@ -4,18 +4,29 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract GenesisHero is AccessControl, ERC1155Burnable, ERC1155Supply {
+    using Strings for uint256;
+    
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    string public name = "Genesis Hero Gear";
+    string public symbol = "GEAR";
 
-    constructor(address defaultAdmin, address minter) ERC1155("") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
+    constructor() ERC1155("") {
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(MINTER_ROLE, _msgSender());
+        _grantRole(URI_SETTER_ROLE, _msgSender());
     }
 
     function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
         _setURI(newuri);
+    }
+
+    function uri(uint256 tokenId) public view virtual override returns (string memory) {
+        string memory uri = super.uri(tokenId);
+        return string(abi.encodePacked(uri, tokenId.toString()));
     }
 
     function mint(address account, uint256 id, uint256 amount, bytes memory data)
